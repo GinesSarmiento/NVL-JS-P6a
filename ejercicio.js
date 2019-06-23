42aa835209d33f88ba931b25c1381c3134c8b528
Ginés Sarmiento <gines.sarmiento@gmail.com>
2019-06-23 21:55:23 +0200

    crea ejercicio.js

---
+++ ejercicio.js
@@ -0,0 +1,5 @@
+var nombre = 'Ginés Sarmiento Fernández'
+var edad = 31
+var dias = ['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo']
+var gravedad = 9.80665
+var impares =[1,3,5]
